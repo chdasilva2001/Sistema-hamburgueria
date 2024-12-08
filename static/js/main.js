a605e28239faa3932d88a256a1b476fc80c509a8
Roberto Freitas <robertosouzafreitas@gmail.com>
2024-12-07 23:03:54 -0300

    Criação do modal de informações e do modal do pedido

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,33 @@
 
 
 
-const switchModal = () => {
-    const modal = document.querySelector('.modal')
-    const actualStyle = modal.style.display
-    if(actualStyle == 'block') {
-      modal.style.display = 'none'
-    }
-    else {
-      modal.style.display = 'block'
-    }
-  }
-  
-  const btn = document.querySelector('.modalBtn')
-  btn.addEventListener('click', switchModal)
-  
-  window.onclick = function(event) {
+  const switchModal = () => {
       const modal = document.querySelector('.modal')
-    if (event.target == modal) {
-      switchModal()
+      const actualStyle = modal.style.display
+      if(actualStyle == 'block') {
+        modal.style.display = 'none'
+      }
+      else {
+        modal.style.display = 'block'
+      }
     }
-  }
+    
+    const btn = document.querySelector('.modalBtn')
+    btn.addEventListener('click', switchModal)
+    
+    window.onclick = function(event) {
+        const modal = document.querySelector('.modal')
+      if (event.target == modal) {
+        switchModal()
+      }
+    }
 
- 
+const BtnFooter = document.querySelector('.BntAdd') 
+const Footer = document.querySelector('.Nav-Footer')
 
+function Produto (){
+  Footer.style.display = 'flex'
 
-
+}
+Footer.addEventListener('click',Produto)
   
