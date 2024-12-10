44555fc0ce46d3a2a05c0f758758bc513a2f99ab
Carlos Henrique da Silva <c.henrique19772001@gmail.com>
2024-12-10 20:20:32 -0300

    alteração views

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.core.paginator import Paginator
-from .models import Produto, Pedido, ItemPedido
+from .models import Produto, Pedido, ItemPedido, Cliente
 
 
 
@@ -52,12 +52,32 @@ def adicionar_ao_carrinho(request, produto_id):
 def exibir_carrinho(request):
     session_id = request.session.session_key
     if not session_id:
-        return render(request, 'Pedido.html', {'itens': []})
+        return render(request, 'carrinho.html', {'itens': []})
 
     itens = ItemPedido.objects.filter(session_id=session_id, pedido__isnull=True)
     total = sum(item.total for item in itens)
 
-    return render(request, 'Pedido.html', {'itens': itens, 'total': total})
+    return render(request, 'carrinho.html', {'itens': itens, 'total': total})
 
 
 
+def finalizar_pedido(request): 
+    if request.method == 'POST': 
+        cliente = get_object_or_404(Cliente, id= request.POST.get("cliente_id"))
+
+        pedido = Pedido.objects.create(cliente=cliente)
+
+        session_id = request.session.session_key
+        itens = ItemPedido.objects.filter(session_id=session_id, pedido__isnull=True)
+
+        for item in itens: 
+            item.pedido = pedido 
+            item.session_id = none 
+            item.save()
+
+
+            itens.delete()
+
+
+
+            return redirect('whatsapp')
